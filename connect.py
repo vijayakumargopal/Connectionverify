64f719f64076e0652686f3acfff59b2cc2f9b954
vijayakumargopal <75659118+vijayakumargopal@users.noreply.github.com>
2021-06-07 22:26:37 +0530

    SortingArray
    created the .py file for sorting array

--- connect.py
+++ connect.py
@@ -1,5 +1,12 @@
-print("connection")
-print("should did")
-print("This is what check whether working or not")
-print("branch trail")
-print("This is vijayakumar and i am forking so i am trying this please ignore if you dont want")
+## am trying to find the solution for sorting the list using python
+
+inputList = [21, 14, 8, 45, 26, 37, 48, 12, 14]
+for x in range(len(inputList)) :
+    minimum = inputList[x]
+    for y in range(x, len(inputList)):
+        if(minimum > inputList[y]) :
+            minimum = inputList[y]
+            temp = y
+    inputList[x] , inputList[temp] = minimum, inputList[x]
+
+print(inputList)
